49cd3695e916c8cc0ee7dba0c487d4e5e48af863
OzgurKaptann <ozgrkptn@gmail.com>
2026-02-23 23:54:24 +0300

    chore: clean repo (remove env, cache, dbt target artifacts)

--- dbt/target/compiled/marketpulse/models/marts/dim_coin.sql
+++
@@ -1,6 +0,0 @@
-select distinct
-  coin_id,
-  symbol,
-  name
-from "marketpulse"."analytics_staging"."stg_coin_markets"
-where coin_id is not null
--- dbt/target/run/marketpulse/models/marts/dim_coin.sql
+++
@@ -1,7 +0,0 @@
-create or replace view "marketpulse"."analytics_marts"."dim_coin__dbt_tmp" as
-  select distinct
-  coin_id,
-  symbol,
-  name
-from "marketpulse"."analytics_staging"."stg_coin_markets"
-where coin_id is not null

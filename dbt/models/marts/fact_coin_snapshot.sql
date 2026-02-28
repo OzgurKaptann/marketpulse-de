58db8903fafc7072a09b88cd3da22326422d56db
OzgurKaptann <ozgrkptn@gmail.com>
2026-03-01 02:35:54 +0300

    Standardize schemas and make fact_coin_snapshot incremental

--- dbt/models/marts/fact_coin_snapshot.sql
+++ dbt/models/marts/fact_coin_snapshot.sql
@@ -1,5 +1,10 @@
-with base as (
+{{ config(
+    materialized='incremental',
+    unique_key="coin_id || '-' || as_of_ts::text || '-' || vs_currency || '-' || page::text || '-' || per_page::text",
+    on_schema_change='sync_all_columns'
+) }}
 
+with base as (
     select
         as_of_ts,
         coin_id,
@@ -13,19 +18,11 @@ with base as (
         page,
         per_page
     from {{ ref('stg_coin_markets') }}
-
 )
 
-select
-    as_of_ts,
-    coin_id,
-    current_price,
-    market_cap,
-    total_volume,
-    market_cap_rank,
-    last_updated,
-    source,
-    vs_currency,
-    page,
-    per_page
+select *
 from base
+
+{% if is_incremental() %}
+where as_of_ts > (select coalesce(max(as_of_ts), '1970-01-01'::timestamptz) from {{ this }})
+{% endif %}
